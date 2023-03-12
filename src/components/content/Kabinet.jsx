67a77d4cfb00e1e0343f9b61b1523e166fb0b215
Jordi Irawan <jordirwn@gmail.com>
2023-03-12 21:52:57 +0700

    🐳 chore: create kabinet card

--- src/components/content/Kabinet.jsx
+++ src/components/content/Kabinet.jsx
@@ -1,9 +1,31 @@
 import { Box, Card, CardBody, CardHeader, Center, Container, Flex, Heading, HStack, Text, VStack } from '@chakra-ui/react'
-import { FaBraille } from 'react-icons/fa'
-import { MdOutlineEnergySavingsLeaf } from 'react-icons/md'
 import { RiTeamLine } from 'react-icons/ri'
-import { TbBrandSupernova } from 'react-icons/tb'
 import KabinetBlob from '../blobs/KabinetBlob'
+
+function KabinetCard({ title, desc }) {
+  return (
+    <Card pos={'relative'} maxW={'xs'} w={'2xs'} variant={'outline'}>
+      <Center>
+        <Box pos={'absolute'} color={'orange.50'} w={'28'} top={0}>
+          <KabinetBlob />
+        </Box>
+      </Center>
+      <CardHeader color={'orange.500'} zIndex={1}>
+        <Center>
+          <RiTeamLine size={60} />
+        </Center>
+      </CardHeader>
+      <CardBody pt={0}>
+        <Heading fontSize={'2xl'} mb={1} color={'gray.700'} textAlign={'center'} letterSpacing={'wide'}>
+          {title}
+        </Heading>
+        <Text align={'center'} color={'gray.600'} letterSpacing={'wide'}>
+          {desc}
+        </Text>
+      </CardBody>
+    </Card>
+  )
+}
 
 function Kabinet() {
   return (
@@ -16,86 +38,21 @@ function Kabinet() {
           Kontribusi Kebaikan
         </Heading>
         <Flex align={'unset'} justify={'center'} pt={6} gap={4} wrap={'wrap'}>
-          <Card pos={'relative'} maxW={'xs'} w={'2xs'} variant={'outline'}>
-            <Center>
-              <Box pos={'absolute'} color={'orange.50'} w={'28'} top={0}>
-                <KabinetBlob />
-              </Box>
-            </Center>
-            <CardHeader color={'orange.500'} zIndex={1}>
-              <Center>
-                <RiTeamLine size={60} />
-              </Center>
-            </CardHeader>
-            <CardBody pt={0}>
-              <Heading fontSize={'2xl'} mb={1} color={'gray.700'} textAlign={'center'} letterSpacing={'wide'}>
-                Kolaborasi
-              </Heading>
-              <Text align={'center'} color={'gray.600'} letterSpacing={'wide'}>
-                Dimana LDK JS UPR mampu meningkatkan kolaborasi yang baik diinternal antar pengurus maupun eksternal dan stakeholder lain dalam mengoptimalkan program
-              </Text>
-            </CardBody>
-          </Card>
-          <Card pos={'relative'} maxW={'xs'} w={'2xs'} variant={'outline'}>
-            <Center>
-              <Box pos={'absolute'} color={'orange.50'} w={'28'} top={0}>
-                <KabinetBlob />
-              </Box>
-            </Center>
-            <CardHeader color={'orange.500'} zIndex={1}>
-              <Center>
-                <TbBrandSupernova size={60} />
-              </Center>
-            </CardHeader>
-            <CardBody pt={0}>
-              <Heading fontSize={'2xl'} mb={1} color={'gray.700'} textAlign={'center'} letterSpacing={'wide'}>
-                Unggul
-              </Heading>
-              <Text align={'center'} color={'gray.600'} letterSpacing={'wide'}>
-                Melakukan sesuatu dengan hasil yang terbaik kreatif, inovatif, dan berkelanjutan.
-              </Text>
-            </CardBody>
-          </Card>
-          <Card pos={'relative'} maxW={'xs'} w={'2xs'} variant={'outline'}>
-            <Center>
-              <Box pos={'absolute'} color={'orange.50'} w={'28'} top={0}>
-                <KabinetBlob />
-              </Box>
-            </Center>
-            <CardHeader color={'orange.500'} zIndex={1}>
-              <Center>
-                <FaBraille size={60} />
-              </Center>
-            </CardHeader>
-            <CardBody pt={0}>
-              <Heading fontSize={'2xl'} mb={1} color={'gray.700'} textAlign={'center'} letterSpacing={'wide'}>
-                Integritas
-              </Heading>
-              <Text align={'center'} color={'gray.600'} letterSpacing={'wide'}>
-                Kader yang mampu selaras antara perbuatan dan perkataan sesuai etika, dan moral Islam Mewujudkan kebijaksanaan dalam bertindak, dan bertanggung jawab terhadap amanah
-              </Text>
-            </CardBody>
-          </Card>
-          <Card pos={'relative'} maxW={'xs'} w={'2xs'} variant={'outline'}>
-            <Center>
-              <Box pos={'absolute'} color={'orange.50'} w={'28'} top={0}>
-                <KabinetBlob />
-              </Box>
-            </Center>
-            <CardHeader color={'orange.500'} zIndex={1}>
-              <Center>
-                <MdOutlineEnergySavingsLeaf size={60} />
-              </Center>
-            </CardHeader>
-            <CardBody pt={0}>
-              <Heading fontSize={'2xl'} mb={1} color={'gray.700'} textAlign={'center'} letterSpacing={'wide'}>
-                Bersinegri
-              </Heading>
-              <Text align={'center'} color={'gray.600'} letterSpacing={'wide'}>
-                Menjalin kerja sama yang bersinergis sebagai bentuk dari sebuah proses atau interaksi yang menghasilkan suatu keseimbangan yang harmonis sehingga bisa menghasilkan sesuatu yang optimum
-              </Text>
-            </CardBody>
-          </Card>
+          <KabinetCard
+            title={'Kolaborasi'}
+            desc={'Dimana LDK JS UPR mampu meningkatkan kolaborasi yang baik diinternal antar pengurus maupun eksternal dan stakeholder lain dalam mengoptimalkan program'}
+          />
+          <KabinetCard title={'Unggul'} desc={'Melakukan sesuatu dengan hasil yang terbaik kreatif, inovatif, dan berkelanjutan'} />
+          <KabinetCard
+            title={'Integritas'}
+            desc={'Kader yang mampu selaras antara perbuatan dan perkataan sesuai etika, dan moral Islam Mewujudkan kebijaksanaan dalam bertindak, dan bertanggung jawab terhadap amanah'}
+          />
+          <KabinetCard
+            title={'Bersinegri'}
+            desc={
+              'Menjalin kerja sama yang bersinergis sebagai bentuk dari sebuah proses atau interaksi yang menghasilkan suatu keseimbangan yang harmonis sehingga bisa menghasilkan sesuatu yang optimum'
+            }
+          />
         </Flex>
       </VStack>
       <HStack wrap={'wrap'}>{/* <Box></Box> */}</HStack>
