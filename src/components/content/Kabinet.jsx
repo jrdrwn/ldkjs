2ac645a7b667cd6bf5a27a215d95c44b78ef820e
Jordi Irawan <jordirwn@gmail.com>
2023-03-12 22:22:53 +0700

    🐞 fix: same icons

--- src/components/content/Kabinet.jsx
+++ src/components/content/Kabinet.jsx
@@ -1,8 +1,11 @@
 import { Box, Card, CardBody, CardHeader, Center, Container, Flex, Heading, HStack, Text, VStack } from '@chakra-ui/react'
+import { FaBraille } from 'react-icons/fa'
+import { MdOutlineEnergySavingsLeaf } from 'react-icons/md'
 import { RiTeamLine } from 'react-icons/ri'
+import { TbBrandSupernova } from 'react-icons/tb'
 import KabinetBlob from '../blobs/KabinetBlob'
 
-function KabinetCard({ title, desc }) {
+function KabinetCard({ title, desc, icon }) {
   return (
     <Card pos={'relative'} maxW={'xs'} w={'2xs'} variant={'outline'}>
       <Center>
@@ -11,9 +14,7 @@ function KabinetCard({ title, desc }) {
         </Box>
       </Center>
       <CardHeader color={'orange.500'} zIndex={1}>
-        <Center>
-          <RiTeamLine size={60} />
-        </Center>
+        <Center>{icon}</Center>
       </CardHeader>
       <CardBody pt={0}>
         <Heading fontSize={{ base: 'xl', md: '2xl' }} mb={1} color={'gray.700'} textAlign={'center'} letterSpacing={'wide'}>
@@ -41,17 +42,20 @@ function Kabinet() {
           <KabinetCard
             title={'Kolaborasi'}
             desc={'Dimana LDK JS UPR mampu meningkatkan kolaborasi yang baik diinternal antar pengurus maupun eksternal dan stakeholder lain dalam mengoptimalkan program'}
+            icon={<RiTeamLine size={60} />}
           />
-          <KabinetCard title={'Unggul'} desc={'Melakukan sesuatu dengan hasil yang terbaik kreatif, inovatif, dan berkelanjutan'} />
+          <KabinetCard title={'Unggul'} desc={'Melakukan sesuatu dengan hasil yang terbaik kreatif, inovatif, dan berkelanjutan'} icon={<TbBrandSupernova size={60} />} />
           <KabinetCard
             title={'Integritas'}
             desc={'Kader yang mampu selaras antara perbuatan dan perkataan sesuai etika, dan moral Islam Mewujudkan kebijaksanaan dalam bertindak, dan bertanggung jawab terhadap amanah'}
+            icon={<FaBraille size={60} />}
           />
           <KabinetCard
             title={'Bersinegri'}
             desc={
               'Menjalin kerja sama yang bersinergis sebagai bentuk dari sebuah proses atau interaksi yang menghasilkan suatu keseimbangan yang harmonis sehingga bisa menghasilkan sesuatu yang optimum'
             }
+            icon={<MdOutlineEnergySavingsLeaf size={60} />}
           />
         </Flex>
       </VStack>
