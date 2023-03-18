7a20659de2692e29de47243b4402560c3a3f8fa0
Jordi Irawan <jordirwn@gmail.com>
2023-03-18 21:47:45 +0700

    ✨ feat: tambah testimoni

---
+++ src/components/content/Testimoni.jsx
@@ -0,0 +1,23 @@
+import { Avatar, Box, Container, Heading, Text, VStack } from '@chakra-ui/react'
+
+export default function Testimoni() {
+  return (
+    <Box px={2}>
+      <Container maxW={'container.xl'} p={{ base: 4, md: 10 }} bgColor={'orange.50'} shadow={'md'} my={10} rounded={'3xl'} border={'1px'} borderColor={'orange.500'}>
+        <VStack>
+          <Heading fontSize={{ base: 'lg', md: '2xl' }} align={'center'} fontWeight={'semibold'} lineHeight={'tall'} mb={2} color={'gray.700'}>
+            "Saya merasa sangat bersyukur dapat menjadi bagian dari LDK JS, dan saya yakin bahwa organisasi ini akan terus tumbuh dan berkembang dengan semangat yang sama kuatnya. Semoga LDK JS dapat
+            terus memberikan manfaat dan kontribusi yang besar bagi masyarakat dan negara."
+          </Heading>
+          <Avatar />
+          <Text fontWeight={'semibold'} color={'orange.500'} align={'center'}>
+            Muhammad Ghozi Anwar
+          </Text>
+          <Text fontWeight={'semibold'} color={'gray.500'} align={'center'}>
+            Ketua Umum
+          </Text>
+        </VStack>
+      </Container>
+    </Box>
+  )
+}
--- src/pages/Beranda.jsx
+++ src/pages/Beranda.jsx
@@ -4,6 +4,7 @@ import NavigationBar from '../components/common/NavigationBar'
 import Kabinet from '../components/content/Kabinet'
 import KegiatanTerbaru from '../components/content/KegiatanTerbaru'
 import ProgramUnggulan from '../components/content/ProgramUnggulan'
+import Quote from '../components/content/Testimoni'
 import SasaranStrategis from '../components/content/SasaranStrategis'
 import VisiMisi from '../components/content/VisiMisi'
 
@@ -17,6 +18,7 @@ function Beranda() {
       <SasaranStrategis />
       <ProgramUnggulan />
       <KegiatanTerbaru />
+      <Quote />
       <Footer />
     </>
   )
