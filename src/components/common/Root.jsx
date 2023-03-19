2cdaf95dadef81992b202cc56b306ccbd5ae56e1
Jordi Irawan <jordirwn@gmail.com>
2023-03-19 10:44:27 +0700

    🐞 fix: scroll to top

--- src/components/common/Root.jsx
+++ src/components/common/Root.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom'
+import ScrollToTop from '../../helpers/ScrollToTop'
 import Footer from './Footer'
 import NavigationBar from './NavigationBar'
 
 export default function Root() {
   return (
     <>
+      <ScrollToTop />
       <NavigationBar />
       <Outlet />
       <Footer />
