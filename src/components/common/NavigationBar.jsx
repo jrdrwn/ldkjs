51fecf7a13be668d9b1127db6809dcbd4b573a65
Jordi Irawan <jordirwn@gmail.com>
2023-03-19 10:45:32 +0700

    🎈 perf: optimazed navbar

--- src/components/common/NavigationBar.jsx
+++ src/components/common/NavigationBar.jsx
@@ -1,9 +1,18 @@
 import { Box, Button, Flex, HStack, IconButton, Image, Show, useBoolean, VStack } from '@chakra-ui/react'
-import { MdArticle, MdClose, MdGroups, MdHome, MdMenu, MdPhone } from 'react-icons/md'
+import { MdClose, MdGroups, MdHome, MdMenu, MdPhone, MdPhotoLibrary } from 'react-icons/md'
+import { NavLink } from 'react-router-dom'
 import brand from '../../assets/brand.png'
 
 export default function NavigationBar() {
   const [flag, setFlag] = useBoolean()
+
+  const pages = [
+    { path: '/', name: 'Beranda', icon: <MdHome size={'18'} /> },
+    { path: '/profil', name: 'Profil', icon: <MdGroups size={'18'} /> },
+    { path: '/galeri', name: 'Galeri', icon: <MdPhotoLibrary size={'18'} /> },
+    { path: '/kontak', name: 'Kontak', icon: <MdPhone size={'18'} /> },
+  ]
+
   return (
     <Flex
       zIndex={'banner'}
@@ -26,35 +35,29 @@ export default function NavigationBar() {
       </Box>
       <Show above="md">
         <HStack gap={4}>
-          <Button variant={'ghost'} colorScheme={'orange'} rounded={'full'} leftIcon={<MdHome size={'18'} />}>
-            Beranda
-          </Button>
-          <Button variant={'ghost'} rounded={'full'} leftIcon={<MdGroups size={'18'} />}>
-            Profil
-          </Button>
-          <Button variant={'ghost'} rounded={'full'} leftIcon={<MdArticle size={'18'} />}>
-            Blog
-          </Button>
-          <Button variant={'ghost'} rounded={'full'} leftIcon={<MdPhone size={'18'} />}>
-            Kontak
-          </Button>
+          {pages.map((page, i) => (
+            <NavLink to={page.path} key={i}>
+              {({ isActive }) => (
+                <Button variant={'ghost'} colorScheme={isActive && 'orange'} rounded={'full'} leftIcon={page.icon}>
+                  {page.name}
+                </Button>
+              )}
+            </NavLink>
+          ))}
         </HStack>
       </Show>
       <Show below="md">
         <IconButton rounded={'full'} icon={flag ? <MdClose size={24} /> : <MdMenu size={24} />} colorScheme={'orange'} onClick={setFlag.toggle} />
         <VStack hidden={!flag} pos={'absolute'} bottom={'-52'} right={'8'} p={2} bgColor={'Background'} border={'1px'} borderColor={'orange'} rounded={'md'} align={'start'} shadow={'md'}>
-          <Button variant={'ghost'} colorScheme={'orange'} rounded={'full'} leftIcon={<MdHome size={'18'} />}>
-            Beranda
-          </Button>
-          <Button variant={'ghost'} rounded={'full'} leftIcon={<MdGroups size={'18'} />}>
-            Profil
-          </Button>
-          <Button variant={'ghost'} rounded={'full'} leftIcon={<MdArticle size={'18'} />}>
-            Blog
-          </Button>
-          <Button variant={'ghost'} rounded={'full'} leftIcon={<MdPhone size={'18'} />}>
-            Kontak
-          </Button>
+          {pages.map((page, i) => (
+            <NavLink to={page.path} key={i}>
+              {({ isActive }) => (
+                <Button variant={'ghost'} colorScheme={isActive && 'orange'} rounded={'full'} leftIcon={page.icon}>
+                  {page.name}
+                </Button>
+              )}
+            </NavLink>
+          ))}
         </VStack>
       </Show>
     </Flex>
